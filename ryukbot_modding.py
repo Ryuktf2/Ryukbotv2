8aa4339e73bd360b17227cc0b5689908b3a761b5
Ryuk <markspannbauer@gmail.com>
2020-07-21 19:30:21 -0500

    added * syntax to RBC

--- ryukbot_modding.py
+++ ryukbot_modding.py
@@ -14,8 +14,8 @@ def getModOptions():
     return modOptions
 
 def checkMods(ryukbot_settings, event, mod_properties):
-    rbcParse = re.compile(r"(run|prefix|suffix) \'(.*)\' on \'(killstreak|bookmark)\' value \'(.*)\'", re.IGNORECASE)
-    if ryukbot_settings["mods"]:
+    rbcParse = re.compile(r"(run|prefix|suffix) \'(.*)\' on \'(killstreak|bookmark|\*)\'( value )?(\'(.*)\')?", re.IGNORECASE)
+    if "mods" in ryukbot_settings:
         for mod in ryukbot_settings["mods"]:
             try: 
                 code = rbcParse.search(mod["code"])
@@ -27,19 +27,29 @@ def checkMods(ryukbot_settings, event, mod_properties):
             # code.group(1) =>  suffix
             # code.group(2) =>  LOL
             # code.group(3) =>  killstreak
-            # code.group(4) =>  69
+            # code.group(4) =>  value   (might not be there)
+            # code.group(5) =>  69      (might not be there)
             
             type = False
-            
-            if (code.group(3).lower() == 'bookmark' and event[1].lower() == 'bookmark'):
-                type = True
-            elif (code.group(3).lower() == 'killstreak' and (event[1].lower() == 'killstreak' or event[1].lower() == 'kill')):
-                type = True
-
-            if type and event[2].lower() == code.group(4).lower():
-                for key in modOptions.keys():
-                    if code.group(1).lower() == key.lower():
-                        mod_properties[modOptions[key]] = code.group(2)
+            valid = False
+                
+            if code.group():
+                if (code.group(3).lower() == 'bookmark' and event[1].lower() == 'bookmark'):
+                    type = True
+                elif (code.group(3).lower() == 'killstreak' and (event[1].lower() == 'killstreak' or event[1].lower() == 'kill')):
+                    type = True
+                elif (code.group(3).lower() == '*'):
+                    type = True
+                
+                if code.group(5) is None:
+                    valid = True
+                elif event[2].lower() == code.group(5).lower() or code.group(5).lower() == '*':
+                    valid = True
+                
+                if type and valid:
+                    for key in modOptions.keys():
+                        if code.group(1).lower() == key.lower():
+                            mod_properties[modOptions[key]] = code.group(2)
                     
                         
     return mod_properties
